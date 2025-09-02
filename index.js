e892b9571e79be86ca98ff7250f4eb21d9b0eaa6
Danilo Sá <141166058+Dannilo13@users.noreply.github.com>
2025-09-02 19:01:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -38,37 +38,38 @@
 
     switch (true) {
         case (heroi1.xp < 1000):
-            console.log(heroi1.name + " é nivel de Ferro!");
+            console.log(heroi1.name + " está no nivel Ferro!");
             break;
         case (heroi2.xp > 1001 && heroi2.xp < 2000): {
-            console.log(heroi2.name + " é nivel de Bronze!");
+            console.log(heroi2.name + " está no nivel Bronze!");
             break;
         }
         case (heroi3.xp > 2001): {
-            console.log(heroi3.name + " é nivel de Prata!");
+            console.log(heroi3.name + " está no nivel Prata!");
             break;
         }
         case (heroi4.xp > 5001): {
-            console.log(heroi4.name + " é nivel de Ouro!");
+            console.log(heroi4.name + "  está no nivel Ouro!");
             break;
         }
         case (heroi5.xp > 7001): {
-            console.log(heroi5.name + " é nivel de Platina!");
+            console.log(heroi5.name + "  está no nivel Platina!");
             break;
         }
         case (heroi6.xp > 8001): {
-            console.log(heroi6.name + " é nivel de Ascendente!");
+            console.log(heroi6.name + " está no nivel Ascendente!");
             break;
         }
-        case (heroi7.xp > 9001): {
-            console.log(heroi7.name + " é nivel de Imortal!");
+       case (heroi7.xp > 9001): {
+            console.log(heroi7.name + " está no nivel Imortal!");
             break;
-        }
-        case (heroi8.xp > 10001): {
-            console.log(heroi8.name + " é nivel de Radiante!");
+       }
+        case (heroi8.xp > 10001):{
+            console.log(heroi8.name + " está no nivel Radiante!");
             break;
             }
     }
     default: {
         console.log("Heroi não encontrado.")
+
     }
